604cc85b623ada07ce0f2c9285271fbf7d275ed6
Pastor Durán <pastorjduran@gmail.com>
2019-11-10 16:46:47 -0300

    Comentarios a EarthQuakeClient
    Se agregan comentarios a clase EarthQuakeClient

--- src/main/java/com/usgs/earthquakes/util/EarthQuakeClient.java
+++ src/main/java/com/usgs/earthquakes/util/EarthQuakeClient.java
@@ -2,6 +2,15 @@ package com.usgs.earthquakes.util;
 
 import com.google.gson.GsonBuilder;
 
+
+/**
+ * Tool to format response to client on json format
+ * @author pastorduran
+ * @project USGS-Earthquakes
+ * @class EarthQuakeClient
+ * @date nov 12, 2019
+ *
+ */
 public class EarthQuakeClient {
 	
 	private static final String DATE_FORMAT = "yyyy-mm-dd";
@@ -10,6 +19,11 @@ public class EarthQuakeClient {
 		
 	}
 	
+	/**
+	 * Method that convert java object to Json String format
+	 * @param object
+	 * @return json object as string format
+	 */
 	public static String getJson(Object object) {
 		return new GsonBuilder().setDateFormat(DATE_FORMAT).serializeNulls().create().toJson(object);
 	}
